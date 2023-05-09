e70ffb663526da029df58c482fc4950f2cf32765
breadman98 <swo98@naver.com>
2023-05-09 20:07:32 +0900

    주석수정

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -23,7 +23,7 @@ const BoardDiv = styled.div<IBoardDivProps & IisVisible>`
   }
 `;
 
-// const BoardDiv = styled.div<IBoardDivProps & IisVisible>` <- 이걸 몰라서 props 인터페이스를 Iprops로 통합해서 인터페이스 줬는데 이상한 에러 막 터졌음
+// const BoardDiv = styled.div<IBoardDivProps & IisVisible>` <- 이걸 몰라서 props 인터페이스를 Iprops로 통합해서 인터페이스 줬는데 막 이상해지고 꼬여서 걍 다 지웠음
 //   display: flex;
 //   flex-wrap: wrap;
 //   border-radius: 0.5rem;
@@ -66,53 +66,3 @@ const Board = () => {
 };
 
 export default Board;
-
-// import React, { useState } from "react";
-// import { useAppSelector } from "../store/hooks";
-// import Tile from "./Tile";
-// import styled from "styled-components";
-
-// interface Iprops {
-//   boardSize: number;
-//   isVisible: boolean;
-// }
-
-// const BoardDiv = styled.div<Iprops>`
-//   display: flex;
-//   flex-wrap: wrap;
-//   border-radius: 0.5rem;
-//   width: ${(props) => props.boardSize * 6.25}rem;
-//   visibility: ${(props) => (props.isVisible ? "visible" : "hidden")};
-// `;
-
-// const VisibleButton = styled.button<Iprops>`
-//   color: ${(props) => (props.isVisible ? "white" : "red")};
-// `;
-
-// const Board = () => {
-//   const [isVisible, setIsVisible] = useState(true);
-
-//   // pre-typed 된 selector와 dispatch 사용
-//   const board = useAppSelector(({ candyCrush: { board } }) => board); // state의 board 잡아오기
-//   const boardSize = useAppSelector(
-//     ({ candyCrush: { boardSize } }) => boardSize // state의 boardSize 잡아오기
-//   );
-
-//   const toggleVisible = () => {
-//     setIsVisible((prev) => !prev);
-//   };
-//   return (
-//     <>
-//       <VisibleButton onClick={toggleVisible} isVisible={isVisible}>
-//         {isVisible ? "Hide" : "Show"} Board
-//       </VisibleButton>
-//       <BoardDiv boardSize={boardSize} isVisible={isVisible}>
-//         {board.map((element: string, index: number) => (
-//           <Tile candy={element} key={index} candyId={index} />
-//         ))}
-//       </BoardDiv>
-//     </>
-//   );
-// };
-
-// export default Board;
